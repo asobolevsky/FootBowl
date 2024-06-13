baa01123b942601dff17ee1e71787c3b5063ea0e
Aleksei Sobolevskii <sobolevsky.alexey@gmail.com>
2024-06-12 22:39:46 -0400

    Initial Commit

---
+++ FootBowl/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  FootBowl
+//
+//  Created by Aleksei Sobolevskii on 2024-06-12.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ FootBowl/FootBowlApp.swift
@@ -0,0 +1,17 @@
+//
+//  FootBowlApp.swift
+//  FootBowl
+//
+//  Created by Aleksei Sobolevskii on 2024-06-12.
+//
+
+import SwiftUI
+
+@main
+struct FootBowlApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
